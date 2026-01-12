c4db7c63859f90132a5b42aa5be63d885d69b591
KnoyanMitsu <yanappuowo@yandex.com>
2026-01-12 20:44:29 +0700

    a

--- lib/src/layout/ace_simple_layout.dart
+++ lib/src/layout/ace_simple_layout.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AceSimpleLayout extends StatelessWidget {
-  const AceSimpleLayout({super.key, required this.childern, required this.title});
+  const AceSimpleLayout({
+    super.key,
+    required this.childern,
+    required this.title,
+  });
 
   final List<Widget> childern;
   final String title;
@@ -20,28 +24,7 @@ class AceSimpleLayout extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Container(
-        margin: const EdgeInsets.all(20),
-        width: double.infinity,
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(color: Colors.grey.shade200, width: 1),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade100,
-              blurRadius: 5,
-              offset: const Offset(0, 2),
-            ),
-          ],
-        ),
-        child: Column(
-          children: [
-            ...childern,
-          ],
-        ),
-      ),
+      body: Column(children: [...childern]),
     );
   }
 }
